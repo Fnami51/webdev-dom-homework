9269fb8621dd7aa7bf4c74cb136b0b9f197704b8
Fnami51 <fnm5160@gmail.com>
2024-03-13 17:25:31 +0200

    pravka

--- script.js
+++ script.js
@@ -13,166 +13,146 @@ function twoDigits(num) {
 
 let comments = [];
 
-const fetchPromise = fetch("https://wedev-api.sky.pro/api/v1/fnami/comments", {
-  method: "GET"
-});
-
-fetchPromise.then((response) => {
-  const jsonPromise = response.json();
-  jsonPromise.then((responseData) => {
-    
-  comments = responseData.comments;
-  renderComments();
+const reguestAPI = () => {
+  fetch("https://wedev-api.sky.pro/api/v1/fnami/comments", {
+    method: "GET"
+  })
+  .then((response) => {
+    return response.json()})
+  .then((responseData) => {
+    comments = responseData.comments;
+    renderComments();
   });
-});
+};
+
+reguestAPI();
 
 const renderComments = () => {
-    const commentHtml = comments.map((comment, index) => {
+  const commentHtml = comments.map((comment, index) => {
 
-      let dateNoFormat = new Date(comment.date)
+  let dateNoFormat = new Date(comment.date)
 
-      let dateString = twoDigits(dateNoFormat.getDate()) + "." + twoDigits(dateNoFormat.getMonth() + 1) + "." + dateNoFormat.getFullYear() + " " + twoDigits(dateNoFormat.getHours()) + ":" + twoDigits(dateNoFormat.getMinutes())
+  let dateString = twoDigits(dateNoFormat.getDate()) + "." + twoDigits(dateNoFormat.getMonth() + 1) + "." + dateNoFormat.getFullYear() + " " + twoDigits(dateNoFormat.getHours()) + ":" + twoDigits(dateNoFormat.getMinutes())
 
-      /*return `<li class="comment" data-index="${index}">
-        <div class="comment-header">
-          <div>${comment.author.name}</div>
-          <div>${comment.date}</div>
+  if (Boolean(comment.isLiked)) {
+    return `<li class="comment" data-index="${index}">
+      <div class="comment-header">
+        <div>${comment.author.name}</div>
+        <div>${dateString}</div>
+      </div>
+      <div class="comment-body">
+        <div class="comment-text">
+          ${comment.text}
         </div>
-        <div class="comment-body">
-          <div class="comment-text">
-            ${comment.text}
-          </div>
+      </div>
+      <div class="comment-footer">
+        <div class="likes">
+          <span class="likes-counter">${comment.likes}</span>
+          <button class="like-button -active-like" data-index="${index}" data-position="${comment.isLiked}"></button>
         </div>
-        <div class="comment-footer">
-          <div class="likes">
-            <span class="likes-counter">${comment.likes}</span>
-            <button class="like-button -active-like" data-index="${index}" data-position="${comment.isLiked}"></button>
-          </div>
+      </div>
+    </li>`;
+  } else {
+    return `<li class="comment" data-index="${index}">
+      <div class="comment-header">
+        <div>${comment.author.name}</div>
+        <div>${dateString}</div>
+      </div>
+      <div class="comment-body">
+        <div class="comment-text">
+          ${comment.text}
         </div>
-      </li>`;*/
-        if (Boolean(comment.isLiked)) {
-            return `<li class="comment" data-index="${index}">
-        <div class="comment-header">
-          <div>${comment.author.name}</div>
-          <div>${dateString}</div>
+      </div>
+      <div class="comment-footer">
+        <div class="likes">
+          <span class="likes-counter">${comment.likes}</span>
+          <button class="like-button" data-index="${index}" data-position="${comment.isLiked}"></button>
         </div>
-        <div class="comment-body">
-          <div class="comment-text">
-            ${comment.text}
-          </div>
-        </div>
-        <div class="comment-footer">
-          <div class="likes">
-            <span class="likes-counter">${comment.likes}</span>
-            <button class="like-button -active-like" data-index="${index}" data-position="${comment.isLiked}"></button>
-          </div>
-        </div>
-      </li>`;
-        } else {
-            return `<li class="comment" data-index="${index}">
-        <div class="comment-header">
-          <div>${comment.author.name}</div>
-          <div>${dateString}</div>
-        </div>
-        <div class="comment-body">
-          <div class="comment-text">
-            ${comment.text}
-          </div>
-        </div>
-        <div class="comment-footer">
-          <div class="likes">
-            <span class="likes-counter">${comment.likes}</span>
-            <button class="like-button" data-index="${index}" data-position="${comment.isLiked}"></button>
-          </div>
-        </div>
-      </li>`;
-        }
-    }).join("");
+      </div>
+    </li>`;
+  }
+  }).join("");
 
-    commentItems.innerHTML = commentHtml;
+  commentItems.innerHTML = commentHtml;
 
-    const buttonLikes = document.querySelectorAll('.like-button')
+  const buttonLikes = document.querySelectorAll('.like-button')
 
-    for (const button of buttonLikes) {
-        button.addEventListener("click", (event) => {
-            event.stopPropagation();
+  for (const button of buttonLikes) {
+    button.addEventListener("click", (event) => {
+      event.stopPropagation();
 
-            const index = Number(button.dataset.index);
+      const index = Number(button.dataset.index);
 
-            let position = Boolean(Number(comments[index].isLiked));
+      let position = Boolean(Number(comments[index].isLiked));
             
-            position = !position;
+      position = !position;
             
-            if (position) {
-                comments[index].likes = comments[index].likes + 1;
-                comments[index].isLiked = Boolean(1);
-                renderComments();
-            }
-            if (!position) {
-                comments[index].likes = comments[index].likes - 1;
-                comments[index].isLiked = Boolean(0);
-                renderComments();
-            }
-        })
-    }
+      if (position) {
+        comments[index].likes = comments[index].likes + 1;
+        comments[index].isLiked = Boolean(1);
+        renderComments();
+      }
+      if (!position) {
+        comments[index].likes = comments[index].likes - 1;
+        comments[index].isLiked = Boolean(0);
+        renderComments();
+      }
+    })
+  }
 
-    const commentElement = document.querySelectorAll(".comment")
+  const commentElement = document.querySelectorAll(".comment")
 
-    for (const comment of commentElement) {
-      comment.addEventListener("click", (event) => {
-        event.stopPropagation();
+  for (const comment of commentElement) {
+    comment.addEventListener("click", (event) => {
+      event.stopPropagation();
 
-        const index = Number(comment.dataset.index);
+      const index = Number(comment.dataset.index);
 
-        textElement.value = `↪️ ${comments[index].text}\n\n${comments[index].author.name}, `;
-      })
-    }
+      textElement.value = `↪️ ${comments[index].text}\n\n${comments[index].author.name}, `;
+     })
+  }
 }
 
 renderComments()
 
 buttonAdd.addEventListener("click", () => {
 
-    nameElement.classList.remove("error");
-    textElement.classList.remove("error");
-    buttonAdd.classList.remove("error-for-button");
+  nameElement.classList.remove("error");
+  textElement.classList.remove("error");
+  buttonAdd.classList.remove("error-for-button");
 
-    let regexp = new RegExp('^[а-яa-zА-ЯA-Z0-9↪️]');
+  let regexp = new RegExp('^[а-яa-zА-ЯA-Z0-9↪️]');
 
-    if (nameElement.value === "" || textElement.value === "" || !regexp.test(nameElement.value) || !regexp.test(textElement.value)) {
-        nameElement.classList.add("error");
-        textElement.classList.add("error");
-        buttonAdd.classList.add("error-for-button");
-        return;
-    }
+  if (nameElement.value === "" || textElement.value === "" || !regexp.test(nameElement.value) || !regexp.test(textElement.value)) {
+    nameElement.classList.add("error");
+    textElement.classList.add("error");
+    buttonAdd.classList.add("error-for-button");
+    return;
+  }
 
-    fetch("https://wedev-api.sky.pro/api/v1/fnami/comments", {
-      method: "POST",
-      body: JSON.stringify({
-        name: nameElement.value,
-        text: textElement.value
-      })
-    }).then((response) => {
-      response.json().then((responseData) => {
-        comments = responseData.comments;
-        renderComments();
-      });
+  buttonAdd.disabled = true;
+  buttonAdd.textContent = "Ожидайте";
+
+  fetch("https://wedev-api.sky.pro/api/v1/fnami/comments", {
+    method: "POST",
+    body: JSON.stringify({
+      name: nameElement.value,
+      text: textElement.value
+    })
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .then((responseData) => {
+    comments = responseData.comments;
+    reguestAPI();
+    buttonAdd.disabled = false;
+    buttonAdd.textContent = "Написать";
+  })
     
-      const jsonPromiseTwo = fetch("https://wedev-api.sky.pro/api/v1/fnami/comments", {
-        method: "GET"
-      }).then((response) => {
-        jsonPromiseTwo = response.json();
+  reguestAPI();
+
+  nameElement.value = "";
+  textElement.value = "";
   
-        jsonPromiseTwo.then((responseData) => {
-          comments = responseData.comments;
-          renderComments();
-        });
-      });
-
-
-    renderComments();
-
-    nameElement.value = "";
-    textElement.value = "";
-});
 });
